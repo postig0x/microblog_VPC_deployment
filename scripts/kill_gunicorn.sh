307744ce36aa8f1cf850140cee5e86d2b67e2378
postig0x <postigo.kura@gmail.com>
2024-09-23 02:30:47 -0400

    update kill_gunicorn to show pid before killing

--- scripts/kill_gunicorn.sh
+++ scripts/kill_gunicorn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #scripts/kill_gunicorn.sh
 
+ps aux | grep gunicorn | grep -v grep | awk "{print $2}" | head -n 1
 kill $(ps aux | grep gunicorn | grep -v grep | awk "{print $2}" | head -n 1)
