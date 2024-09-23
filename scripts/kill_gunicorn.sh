e2ab16c5c1e83fa3662c6a3ae29528ff002b5b81
postig0x <postigo.kura@gmail.com>
2024-09-23 02:33:40 -0400

    kill should work now

--- scripts/kill_gunicorn.sh
+++ scripts/kill_gunicorn.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #scripts/kill_gunicorn.sh
 
-ps aux | grep gunicorn | grep -v grep | awk "{print $2}" | head -n 1
-kill $(ps aux | grep gunicorn | grep -v grep | awk "{print $2}" | head -n 1)
+kill $(ps aux | grep gunicorn | grep -v grep | awk "{print \$2}" | head -n 1)
